a5a40fd61a8e8aaa6587696739262d4288fcd96d
nlson20 <nguyenleson666@gmail.com>
2026-06-21 13:41:17 +0700

    Update README and remove Dashboard page
    - Write README.md.
    - Remove Dashboard page for not being used.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { AuthProvider } from './context/AuthContext';
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
 import HomePage from './pages/HomePage';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
